3a96bc8f0dd425b9ffb495eb043bcc77ed318d11
clsied <55375814+clsied@users.noreply.github.com>
2021-04-03 15:04:39 +0800

    Update models.py

--- Package/models.py
+++ Package/models.py
@@ -1,39 +1,61 @@
 from Package import db
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
-
-# User Post Assocation Table
+# User room Assocation Table
 # https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html#many-to-many
  
-user_post_table = db.Table('user_post_table',
-    db.Column('post_id', db.Integer, db.ForeignKey('posts.id')),
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
+user_room = db.Table('user_room',
+    db.Column('room_id', db.Integer, db.ForeignKey('room.id'),primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'),primary_key=True)
 )
 
 # Model of User
 class User(db.Model):
 
     # Object User's attribute #
-    __tablename__ = 'users'  # means that the data will be in SQLite's 'user' table
     id = db.Column(db.Integer, primary_key=True)    # unique id for the Object User
     name = db.Column(db.String(100), unique=True, nullable=False)   # unique name for the Object User, not be blank
     password = db.Column(db.String(100),nullable=False) # non-unique password for the Object User
-      
+    rate = db.Column(db.Integer,nullable=False) # non-unique number of rating of this Object User
+    roomnum = db.Column(db.Integer,nullable=False) # non-unique number of room-created by this Object User
+    rooms = db.relationship('Room',secondary=user_room,lazy='subquery',
+        backref=db.backref('users', lazy=True))
+
     # initialization
     def __init__(self, name, password):
         self.name = name    # create Object User with name intialized
         self.password = password    # create Object User with name intialized
+        self.rate = 0   # default Object User rating
+        self.roomnum = 0    # default Object User room-created number
 
     
-# Model of Post
-class Post(db.Model):
+# Model of room
+class Room(db.Model):
 
     # Object User's attribute #
-    __tablename__ = 'posts'  # means that the data will be in SQLite's 'post' table
-    id = db.Column(db.Integer, primary_key=True)    # unique id for the Object Post
-    title = db.Column(db.String(100), nullable=False)   # non-unique title for the Object Post
-    content = db.Column(db.Text, nullable=False)    # non-unique content for the Object Post
-
+    id = db.Column(db.Integer, primary_key = True)    # unique id for the Object Room
+    event = db.Column(db.String(100), nullable = False)   # non-unique event for the Object Room
+    time = db.Column(db.String(100), nullable = False)  # non-unique event time for the Object Room
+    location = db.Column(db.String(100), nullable = False) # non-unique event place for the Object Room
+    description = db.Column(db.Text, nullable = False)    # non-unique description for the Object Room
+    pplneed = db.Column(db.Integer, nullable = False)   # non-unique number of people needed for the Object Room, constant
+    pplnow = db.Column(db.Integer, nullable = False)    # non-unique number of people now in the Object Room
+    hostid = db.Column(db.Integer,nullable = False)   # non-unique id of host of the Object Room
+    isfull = db.Column(db.Boolean, nullable = False)    # non-unique isfull of the Object Room
     # initialization
-    def __init__(self,title,content):
-        self.title = title  # create Object Post with title intialized
-        self.content = content  # create Object Post with content intialized
+    def __init__(self,event,time,location,description,pplneed,hostid):
+        self.event = event  # create Object Room with event intialized
+        self.time = time    # create Object Room with event time intialized
+        self.location = location    # create Object Room with event location initialized
+        self.description = description  # create Object Room with description intialized
+        self.pplneed = pplneed  # create Object rRoom with people needed intialized
+        self.pplnow = 1 # default number of people now in Object Room
+        self.hostid = hostid # create Object Room with people needed intialized
+        self.isfull = False # default Boolean of iffull in Object Room
+
+        # increment 1 to host's roomnum
+        User.query.get(self.hostid).roomnum += 1    # notice that at here we dont need to db.session.commit()
+                                                    # since it will db.session.commit() after the Object Room is created
+                                                    # in dummy.py 
+        

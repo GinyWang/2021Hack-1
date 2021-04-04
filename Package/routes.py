495a3796f1b9d35c9c25d1fa9dbed9285a03afdb
clsied <55375814+clsied@users.noreply.github.com>
2021-04-04 22:12:28 +0800

    Update routes.py

--- Package/routes.py
+++ Package/routes.py
@@ -1,27 +1,41 @@
 from flask import render_template, request ,flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, current_user
 from Package import app, db
 from Package.models import User, Room
 
 # main page
-@app.route("/", methods = ['POST','GET'] )
+
+@app.route("/", methods = ['POST', 'GET'])
 def index():
-    # if the frontend use room to backend
-    if request.method == 'POST':
-        return render_template("login.html")
-    return render_template("index.html")
+    return redirect(url_for("room_list"))
 
 
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('room_list'))
     if request.method == 'POST':
+
         username = request.form.get('username')
         password = request.form.get('password')
-        return redirect(url_for("create_room"))
+
+        user = User.query.filter_by(name=username).first()
+        if user.password == password:
+            login_user(user)
+        
+            return redirect(url_for("room_list"))
     return render_template("login.html")
 
 @app.route("/room_list",methods = ['POST', 'GET'])
 def room_list():
+    
+    if request.method == 'POST':
+        if request.form['action'] == 'login':
+            return redirect(url_for("login"))
+        elif request.form['action'] == 'create':
+            return redirect(url_for("create_room"))
+
     obj_R = Room.query.order_by(Room.id).all()
     data_pass = [
         {"event" :obj.event,
@@ -37,6 +51,9 @@ def room_list():
 
 @app.route("/create_room",methods = ['POST', 'GET'])
 def create_room():
+    # if not login then redirect to login page
+    if not current_user.is_authenticated:
+        return redirect(url_for("login"))
     if request.method == 'POST':
         if request.form['submit'] == 'Submit':
             event = request.form.get('Ename')
@@ -46,12 +63,13 @@ def create_room():
             description = request.form.get('descrip')
             
             # dummy poster
-            room = Room(event,time,location,description,pplneed,1)
-            db.session.add(room)
+            room = Room(event,time,location,description,pplneed,current_user.id)
+            current_user.rooms.append(room)
             db.session.commit()
             return redirect(url_for("room_list"))
-
-        elif request.form['reset'] == 'Reset':
-            return render_template("create_room.html")
-
     return render_template("create_room.html")
+
+@app.route("/logout", methods = ['POST', 'GET'])
+def logout():
+    logout_user()
+    return redirect(url_for("room_list"))

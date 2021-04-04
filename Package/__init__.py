87a71f1b5e7112030513c7a69e7f406c13cf2bd8
clsied <55375814+clsied@users.noreply.github.com>
2021-04-04 22:23:07 +0800

    Update __init__.py

--- Package/__init__.py
+++ Package/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
@@ -11,9 +12,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # configurate the database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(current_dir, 'data.sqlite')
-
 db = SQLAlchemy(app)
 
+app.secret_key = 'hackust2021'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+# login_manager.login_view = 'login'
+# login_manager.login_message = 'Please login!'
 
 from Package import routes

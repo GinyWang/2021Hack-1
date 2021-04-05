fe7d63481fc94202d159ef79438964c41ea2baa0
clsied <55375814+clsied@users.noreply.github.com>
2021-04-06 02:03:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,4 @@ from Package import app, db # from __init__ import app db Object
 import os
 
 if __name__ == '__main__':
-    # if system not found data.sqlite, then create an database
-    if not os.path.exists('data.sqlite'):
-        db.create_all()
     app.run(host='localhost',debug=True)
